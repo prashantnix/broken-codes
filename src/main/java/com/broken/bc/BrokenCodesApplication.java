d80dd4c4e608d04361c5350c8364e7e4f35a75f6
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-03-06 21:08:27 +0530

    Initial Commit

---
+++ src/main/java/com/broken/bc/BrokenCodesApplication.java
@@ -0,0 +1,13 @@
+package com.broken.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokenCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokenCodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/broken/bc/BrokenCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broken.bc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokenCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
